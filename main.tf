c1449b13ced2e5678a19c1b81840ac4d0e087fbf
aaltopiiri <aaltopiiri@outlook.com>
2020-09-15 11:21:07 +0300

    Add instances

--- main.tf
+++ main.tf
@@ -140,3 +140,15 @@ resource "aws_alb_listener" "listener_http" {
     type             = "forward"
   }
 }
+
+resource "aws_lb_target_group_attachment" "atachment-1" {
+  target_group_arn = aws_alb_target_group.group.arn
+  target_id        = aws_instance.web-1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "atachment-2" {
+  target_group_arn = aws_alb_target_group.group.arn
+  target_id        = aws_instance.web-2.id
+  port             = 80
+}
