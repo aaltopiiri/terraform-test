7625adc01f4a0777ff6525f9f95745581a971246
aaltopiiri <aaltopiiri@outlook.com>
2020-09-15 10:32:26 +0300

    Add listener

--- main.tf
+++ main.tf
@@ -129,3 +129,14 @@ resource "aws_alb_target_group" "group" {
     port = 80
   }
 }
+
+resource "aws_alb_listener" "listener_http" {
+  load_balancer_arn = aws_alb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_alb_target_group.group.arn
+    type             = "forward"
+  }
+}
